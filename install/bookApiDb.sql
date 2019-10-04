3942f9f25094df1c3f4e1ef6b2373960270227dc
Philippe Haerens <haerphi@gmail.com>
2019-10-04 14:33:38 +0200

    creation db files

---
+++ install/bookApiDb.sql
@@ -0,0 +1,36 @@
+drop table books_has_authors;
+drop table books;
+drop table authors;
+drop table users;
+
+CREATE TABLE "users" (
+  "id" SERIAL UNIQUE,
+  "email" varchar UNIQUE,
+  "password" text,
+  "admin" boolean
+);
+
+CREATE TABLE "books" (
+  "id" SERIAL UNIQUE,
+  "title" varchar,
+  "subTitle" varchar,
+  "editor" int,
+  "format" varchar,
+  "langue" int,
+  "couverture" text,
+  "stock" int
+);
+
+CREATE TABLE "authors" (
+  "id" SERIAL UNIQUE,
+  "name" varchar
+);
+
+CREATE TABLE "books_has_authors" (
+  "id_author" int,
+  "id_book" int
+);
+
+ALTER TABLE "books_has_authors" ADD FOREIGN KEY ("id_author") REFERENCES "authors" ("id");
+
+ALTER TABLE "books_has_authors" ADD FOREIGN KEY ("id_book") REFERENCES "books" ("id");
