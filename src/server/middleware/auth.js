5db3198b68713f172423fd5864c364c003c5babb
Philippe Haerens <haerphi@gmail.com>
2019-10-07 18:39:55 +0200

    écriture dans la bd + register

--- src/server/middleware/auth.js
+++ src/server/middleware/auth.js
@@ -1,9 +1,31 @@
 const jwt = require("jsonwebtoken");
+import bd from "../bd/postgresql";
 
 const secret_key = "banana";
 
-export const register = async (req, res, nect) => {
-  res.send("Did you try to register ?");
+export const register = async (req, res, next) => {
+  if (req.body) {
+    const { email, password } = req.body;
+    if (email && password) {
+      //verify if email already exists
+      let rep = await bd.from("users").where("email", email);
+      if (rep.length < 1) {
+        let rep2 = await bd("users").insert({
+          email: email,
+          password: password
+        });
+        console.log(rep2);
+        res.send(`${email} added to the database`);
+      } else {
+        res.send("This email already exists in the database");
+      }
+    } else {
+      res.send({
+        sucess: false,
+        token: "You need to send an email and a password"
+      });
+    }
+  }
 };
 
 //génération du token
@@ -12,9 +34,22 @@ export const authentification = async (req, res, next) => {
     const { email, password } = req.body;
     if (email && password) {
       //verify if email and password matche
-      //if (TODO)
-      var token = jwt.sign({ email: email }, secret_key);
-      res.send({ sucess: true, token: token });
+      let rep = await bd
+        .from("users")
+        .where("email", email)
+        .where("password", password);
+      if (rep.length > 0) {
+        //if it matches
+        var token = jwt.sign({ email: email, admin: rep[0].admin }, secret_key);
+        res.send({ sucess: true, token: token });
+        await bd
+          .from("users")
+          .where("email", email)
+          .where("password", password)
+          .update({ token: token });
+      } else {
+        res.send("Email and password doesn't match");
+      }
       //ajouter dans la BD le token
       //TODO
     } else {
