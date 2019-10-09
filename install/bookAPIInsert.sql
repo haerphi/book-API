1ab169bf33480434b8a023eb96087e7eb250a37d
Philippe Haerens <haerphi@gmail.com>
2019-10-09 10:05:57 +0200

    heu ?

---
+++ install/bookAPIInsert.sql
@@ -0,0 +1,3 @@
+INSERT INTO books_has_authors (id_author, id_book) VALUES (1, 1);
+INSERT INTO books_has_authors (id_author, id_book) VALUES (2, 2);
+INSERT INTO books_has_authors (id_author, id_book) VALUES (2, 1);
