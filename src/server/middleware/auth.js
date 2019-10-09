ceb0dcbb50ec9836e633371a5ac2bdca371d9f1d
Philippe Haerens <haerphi@gmail.com>
2019-10-09 16:54:05 +0200

    fix de bug dans l'authentification

--- src/server/middleware/auth.js
+++ src/server/middleware/auth.js
@@ -5,19 +5,21 @@ const secret_key = "banana";
 
 export const register = async (req, res, next) => {
   if (req.body) {
-    const { email, password } = req.body;
-    if (email && password) {
+    const { email, password, name } = req.body;
+    if (email && password && name) {
       //verify if email already exists
       let rep = await bd.from("users").where("email", email);
-      if (rep.length < 1) {
+      let rep2 = await bd.from("users").where("name", name);
+      if (rep.length < 1 && rep2.length < 1) {
         let rep2 = await bd("users").insert({
           email: email,
-          password: password
+          password: password,
+          name: name
         });
         console.log(rep2);
         res.send(`${email} added to the database`);
       } else {
-        res.send("This email already exists in the database");
+        res.send("This email/name already exists in the database");
       }
     } else {
       res.send({
