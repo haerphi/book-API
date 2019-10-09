4c64f4b5bd3e03b60ec42274bf0e7af221638032
Philippe Haerens <haerphi@gmail.com>
2019-10-09 22:41:34 +0200

    tentative de connexion avec la bd de heroku

--- src/server/bd/postgresql.js
+++ src/server/bd/postgresql.js
@@ -1,14 +1,33 @@
 const knex = require("knex");
 
-console.log(process.env.DATABASE_URL);
+let user = null;
+let mdp = null;
+let host = null;
+let database = null;
 
+let url = process.env.DATABASE_URL;
+if (url) {
+  url = url.substr(11);
+  user = url.substr(0, url.indexOf(":")); //ok on a récupérer le user
+  url = url.substr(url.indexOf(":") + 1);
+
+  mdp = url.substr(0, url.indexOf("@")); //ok on a récupérer le mdp
+  url = url.substr(url.indexOf("@") + 1);
+
+  host = url.substr(0, url.indexOf(":")); //ok on a récupérer le host
+  url = url.substr(url.indexOf(":") + 1);
+
+  port = url.substr(0, url.indexOf("/")); //ok on a récupérer le port
+  url = url.substr(url.indexOf("/") + 1);
+  database = url; //ok on a récupérer le nom de la database
+}
 module.exports = knex({
   client: "pg",
   version: "7.2",
   connection: {
-    host: "postgres",
-    user: "dev",
-    password: "dev",
-    database: "booksApi"
+    host: host || "postgres",
+    user: user || "dev",
+    password: mdp || "dev",
+    database: database || "booksApi"
   }
 });
